8a915e8672b659c0f5682113a5860b87cabb7831
Дмитрий <demosne@gmail.com>
2021-10-19 12:17:03 +0300

    ch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import "./null_styles.css";
-import "./App.css";
+import { useState } from "react";
+import { TextField } from "@mui/material";
 import Numbers from "./components/numbers";
 import Operators from "./components/operators";
-
-function handleClick(value) {
-  console.log(value);
-}
+import "./App.css";
 
 function App() {
+  const [str, setStr] = useState("");
+  const [prevState, setPrevState] = useState("");
+
+  function doCulc() {
+    console.log(str);
+    setPrevState(str);
+    setStr("");
+  }
+
+  function handleClick(value) {
+    if (value === "=") {
+      doCulc();
+    } else {
+      setStr((prevValue) => prevValue + value);
+    }
+  }
+
   return (
     <div id="wrap">
       <div id="app">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            doCulc();
+          }}
+        >
+          <TextField
+            style={{ width: "100%", marginBottom: 15, fontSize: 30 }}
+            required
+            label={prevState}
+            onChange={(e) => setStr(e.target.value)}
+            value={str}
+          />
+        </form>
         <div id="btns">
           <Numbers handleClick={handleClick} />
           <Operators handleClick={handleClick} />
